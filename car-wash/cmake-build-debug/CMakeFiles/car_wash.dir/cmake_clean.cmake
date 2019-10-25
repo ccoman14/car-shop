021e41b43735e3da421ec0ba6fcd5a1e5d13ef36
Claudiu Coman <comanclaudiu14@yahoo.com>
2019-10-25 15:05:07 +0300

    crated hello world project

---
+++ car-wash/cmake-build-debug/CMakeFiles/car_wash.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car_wash.dir/main.c.obj"
+  "car_wash.exe"
+  "car_wash.exe.manifest"
+  "car_wash.pdb"
+  "libcar_wash.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/car_wash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ car-wash/cmake-build-debug/CMakeFiles/car_wash.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for car_wash.
+# This may be replaced when dependencies are built.
